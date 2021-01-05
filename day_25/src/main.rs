574364667abef31ca4bcbd62ec34f63c379dac41
Maximilian Stock <mxmlnstock@googlemail.com>
2021-01-05 01:34:12 +0100

    refactor: simplify Day 25

--- day_25/src/main.rs
+++ day_25/src/main.rs
@@ -1,4 +1,3 @@
-use mod_exp::mod_exp;
 use std::collections::HashMap;
 
 fn main() {
@@ -9,12 +8,20 @@ fn main() {
     println!("Part 1: {}", result);
 }
 
+fn mod_exp(g: usize, e: usize, modulo: usize) -> usize {
+    let mut c = 1;
+    for _ in 0..e {
+        c = c * g % modulo;
+    }
+    c
+}
+
 fn part1(pkey1: usize, pkey2: usize) -> Result<usize, ()> {
     let loop_size_door = babystep_giantstep(7, 20201227, pkey2).unwrap();
     Ok(mod_exp(pkey1, loop_size_door, 20201227))
 }
 
-/// See https://en.wikipedia.org/wiki/Baby-step_giant-step
+/// See https://en.wikipedia.org/wiki/Baby-step_giant-step for calculating discrete logarithms
 #[allow(clippy::many_single_char_names)]
 fn babystep_giantstep(g: usize, r#mod: usize, h: usize) -> Option<usize> {
     let mut table = HashMap::new();
@@ -24,7 +31,7 @@ fn babystep_giantstep(g: usize, r#mod: usize, h: usize) -> Option<usize> {
     let mut e = 1;
     for j in 0..m {
         table.insert(e, j);
-        e = (e * g) % r#mod;
+        e = e * g % r#mod;
     }
 
     let factor = mod_exp(g, r#mod - m - 1, r#mod);
